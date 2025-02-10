971d3510799d949c2d5a698b850392ebf5a5a2d2
Pierre Brunelle <pierrebrunelleai@gmail.com>
2025-02-10 01:44:33 +0000

    Add Amazon Linux

---
+++ install_amazon_linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -s $PWD/.gitconfig ~/.gitconfig 
+cat $PWD/.bash_profile >> ~/.bash_profile
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ln -s $PWD/.gitconfig ~/.gitconfig 
+ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.bash_profile ~/
 
 echo "source ~/.bash_profile" >> ~/.bashrc
